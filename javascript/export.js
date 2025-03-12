612a18b75a7a3f273a9332158ac9256ec20916b7
PiotrJesey <167005812+PiotrJesey@users.noreply.github.com>
2025-03-12 10:33:23 +0000

    export dropdown

--- javascript/export.js
+++ javascript/export.js
@@ -3,14 +3,15 @@ async function sendToPowerAutomate() {
    // let surname = document.getElementById("surname").value;
     let email = document.getElementById("email").value;
     let score = document.getElementById("score").innerText;
-    let dropdown1 = document.getElementById("dropdown1").value;
+    let dropdown1 = document.getElementById("dropdown1");
+    let selectedText = dropdown1.options[dropdown1.selectedIndex].text;
 
     let data = {
         Name: name,
        // Surname: surname,
         Email: email,
         Score: score,
-        Dropdown1: dropdown1
+        Dropdown1: selectedText
     };
 
     let response = await fetch("https://prod-28.westeurope.logic.azure.com:443/workflows/0cf0be27d4504b8aa24c7389183d902c/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=8Pj7td5CpvRMxlaDrj1ATtbrywxcvgq_JoqHiJ1kyHo", {
